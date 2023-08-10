cc77c4ed4d607f81d48740485e64d2d37091e5a8
vitkondz <vitanhv26@gmail.com>
2023-08-10 17:06:33 +0700

    create model and migration

---
+++ Story/database/migrations/2023_08_10_155221_create_stories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->increments('story_id');
+            $table->string('story_name');
+            $table->integer('author_id')->unique();
+            $table->string('author_name');
+            $table->string('category');
+            $table->string('thumb');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stories');
+    }
+};
---
+++ Story/database/migrations/2023_08_10_155450_create_pages_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('page_number');
+            $table->integer('story_id');
+            $table->string('image_background');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pages');
+    }
+};
